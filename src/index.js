c99f5849b4eb8f82203aa57f5d969cfdcca9a886
Vladimir Belich <belichvladimir@mail.ru>
2022-11-28 17:52:21 +0300

    3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-function info() {
-    const m = new Map();
-    m.set('name', 'ajs');
-    m.set('version', '1.0.0');
-    return m;
-  }
+// function info() {
+//     const m = new Map();
+//     m.set('name', 'ajs');
+//     m.set('version', '1.0.0');
+//     return m;
+//   }
   
-  export default {
-    info
-  };
+//   export default {
+//     info
+//   };
+
+const ajs = require('@vsgenius/js-adv-less3');
+
+console.log(ajs.info());
